5aab1b9d6a528fa9f71e004a507c54f830777c22
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-04 15:48:29 +0530

    Modify the pyython_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -353,3 +353,18 @@ while True:
 # here key is defined by lambda and the data is sorted by element priority 0>1>2 in accending order
 lst.sort(key=lambda x: (x[0], int(x[1]), int(x[2])))
 print(lst)
+
+# 20 Define a class with a generator which can iterate the numbers, which are divisible by 7, between a given range 0 and n.
+# Suppose the following input is supplied to the program:7
+# Then, the output should be:0 7
+
+
+class MyGen():
+    def div_by_seven(self, num):
+        for number in range(0, num + 1):
+            if number % 7 == 0:
+                yield number
+
+
+for i in MyGen().div_by_seven(int(input("Enter a number... "))):
+    print(i)
