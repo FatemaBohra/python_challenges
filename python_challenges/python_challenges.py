b3000eeef8638c1b9a964cfa06d3511810f8a7b9
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-04 15:50:30 +0530

    Modify the python_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -368,3 +368,29 @@ class MyGen():
 
 for i in MyGen().div_by_seven(int(input("Enter a number... "))):
     print(i)
+
+# 21 A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps.
+# Please write a program to compute the euclidean distance from current position after a sequence of movement and original point. If the distance is a float, then just print the nearest integer.
+# Example: If the following tuples are given as input to the program: UP 5 DOWN 3 LEFT 3 RIGHT 2
+# Then, the output of the program should be: 2
+# Here distance indicates to euclidean distance.Import math module to use sqrt function.
+
+x, y = 0, 0  # cartesian plane
+while True:
+    movement = input().split()
+    if not movement:
+        break
+    if movement[0] == 'UP':
+        y = y + int(movement[1])
+    elif movement[0] == 'DOWN':
+        y = y - int(movement[1])
+    elif movement[0] == 'LEFT':
+        x = x - int(movement[1])
+    elif movement[0] == 'RIGHT':
+        x = x + int(movement[1])
+    else:
+        pass
+
+# euclidean distance = square root of (x^2+y^2) and rounding it to nearest integer
+distance = round(math.sqrt(x**2 + y**2))
+print(distance)
