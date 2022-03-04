1e6b94626e5f4471c94ded83f03e26c794986e98
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-04 15:52:05 +0530

    Modify the python_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -420,3 +420,20 @@ def cal_square_val(number):
 
 print(cal_square_val(2))
 print(cal_square_val(7))
+
+# 24 Python has many built-in functions, and if you do not know how to use it, you can read document online or find some books. But Python has a built-in document function for every built-in functions.
+# Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+# And add document for your own function, The built-in document method is __doc__
+
+print(abs.__doc__)
+print(int.__doc__)
+print(str.__doc__)
+print(sorted.__doc__)
+
+
+def power(num, pow):
+    return num**pow
+
+
+print(power(3, 4))
+print(pow.__doc__)
