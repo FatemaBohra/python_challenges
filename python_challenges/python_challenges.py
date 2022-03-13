5285d42c28ebe580ec8f01b9164230c7f68f2c11
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-13 11:23:47 +0530

    Modify the python_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -972,3 +972,10 @@ values = []
 for i in div_by_5_7(num):
     values.append(str(i))
 print(','.join(values))
+
+# 65 Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+# Hints Use "assert expression" to make assertion.
+
+li = [2, 4, 5, 6, 8]
+for i in li:
+    assert i % 2 == 0
