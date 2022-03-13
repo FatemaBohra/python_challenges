519597f9fd6195282d4678d50b582fac4db1c2d2
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-13 11:22:02 +0530

    Modify the python_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -919,3 +919,39 @@ def f(n):
 
 num = int(input("Enter a num: "))
 print(f(num))
+
+# 62 The Fibonacci Sequence is computed based on the following formula: f(n)=0 if n=0, f(n)=1 if n=1, f(n)=f(n-1)+f(n-2) if n>1
+# Please write a program to compute the value of f(n) with a given n input by console.
+# Example: 7
+# Then, the output of the program should be: 0,1,1,2,3,5,8,13
+
+
+def f(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return f(n-1) + f(n-2)
+
+
+num = int(input("Enter a num: "))
+values = (str(f(i)) for i in range(0, num + 1))
+print(','.join(values))
+
+# 63 Please write a program using generator to print the even numbers between 0 and n in comma separated form while n is input by console.
+# Example: 10
+# Then, the output of the program should be: 0,2,4,6,8,10
+
+
+def even_value(n):
+    for number in range(0, n + 1):
+        if number % 2 == 0:
+            yield number
+
+
+num = int(input("Enter num: "))
+values = []
+for i in even_value(num):
+    values.append(str(i))
+print(','.join(values))
