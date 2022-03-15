cd6453fd00fab9f6b05a46e7fc3caa7eb175e0f3
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-15 16:52:36 +0530

    Modify the python_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -1189,3 +1189,15 @@ def remove_num_div_by_5_7():
 
 
 print(remove_num_div_by_5_7())
+
+# 82 By using list comprehension, please write a program to print the list after removing the 0th, 2nd, 4th,6th numbers in [12,24,35,70,88,120,155].
+
+
+def removing_0th_2nd_4th_6th_num():
+    li = [12, 24, 35, 70, 88, 120, 155]
+    li = [li[i] for i in range(len(li)) if i %
+          2 != 0 and i <= 6]  # i = 0, 1, 2, 3, 4, 5, 6
+    return(li)
+
+
+print(removing_0th_2nd_4th_6th_num())
