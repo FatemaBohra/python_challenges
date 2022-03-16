73431598bc52e01161368418ca5c287b432d327a
Fatema Bohra <fatema.bohra2003@gmail.com>
2022-03-16 10:18:01 +0530

    Modify the python_challenges file

--- python_challenges/python_challenges.py
+++ python_challenges/python_challenges.py
@@ -1364,3 +1364,20 @@ print(even_indexes())
 # Hint: Use itertools.permutations() to get permutations of list
 
 print(list(itertools.permutations([1, 2, 3])))
+
+# 94 Write a program to solve a classic ancient Chinese puzzle: We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have?
+
+
+def solve(numheads, numlegs):
+    ns = 'No solutions!'
+    for i in range(numheads+1):
+        j = numheads-i
+        if 2*i + 4*j == numlegs:
+            return i, j
+    return ns, ns
+
+
+numheads = 35
+numlegs = 94
+solutions = solve(numheads, numlegs)
+print(solutions)
